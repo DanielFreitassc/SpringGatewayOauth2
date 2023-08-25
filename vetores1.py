057ea6dfcc5e24905896891a771b070963c9c0dc
Daniel Freitas <129224303+DanielFreitassc@users.noreply.github.com>
2023-08-25 18:45:00 -0300

    Add files via upload

---
+++ vetores1.py
@@ -0,0 +1,30 @@
+#Saudação para o nome do usuario.
+nome = input("Digite seu nome: ")
+print("Ola", nome)
+#Mexendo com coleção
+lista_idades = [20,30,40]
+lista_idades.append(50)
+
+print(lista_idades)
+#Mexendo com coleção usando inserir
+lista_idades = [20,30,40]
+lista_idades.insert(0,10)
+
+print(lista_idades)
+#Mexendo com coleção usando o remover
+lista_idades = [20,30,40]
+lista_idades.remove(20)
+
+print(lista_idades)
+#Mexendo com coleção substituindo valores
+lista_idades = [20,30,40]
+lista_idades[0] = 10
+lista_idades[1] = 20
+lista_idades[2] = 30
+
+print(lista_idades)
+#Mexendo com coleção botando em ordem
+lista_idades = [5,4,3,2,1]
+lista_idades.sort()
+print(lista_idades)
+
